fbca92695038cd0c31eaff563c526e01018baf0c
zesardine <91370357+zesardine@users.noreply.github.com>
2021-09-25 12:11:48 +0200

    Added closing tag, maybe that'll fix formatting

--- AllBookmarklets.js
+++ AllBookmarklets.js
@@ -38,7 +38,7 @@ void 0;
 //NOTEPAD SIMPLE
 
 data:text/html;charset=utf-8,
-<html contenteditable style="background-color:%23222; padding: 5%; color: %23EEE; font-family: Courier New; font-size: 20px;">
+<html contenteditable style="background-color:%23222; padding: 5%; color: %23EEE; font-family: Courier New; font-size: 20px;"></html>
 
 
 //NOTEPAD BETTER
